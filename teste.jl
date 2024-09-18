66fa586d49aafc2f23196eeea290040a761a0a66
mickpires <83048320+mickpires@users.noreply.github.com>
2024-09-17 23:23:51 -0300

    madm

--- teste.jl
+++ teste.jl
@@ -1,3 +1,21 @@
-using Plots
-using Optim
-include("markov.jl")
+using Distributions
+using Random
+
+function y(x)
+    return x^2
+end
+
+
+a = 0
+
+b = 2
+iter = 2000
+
+yy = []
+
+for i ∈ 1:iter
+    x = rand(Uniform(a,b))
+    push!(yy,y(x))
+end
+
+println("A área é ", (b-a)*mean(yy))
